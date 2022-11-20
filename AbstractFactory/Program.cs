73bd3edf22c1b235432b8e5d31e06064ac4e8044
kristjankangro <87314450+kristjankangro@users.noreply.github.com>
2022-11-20 12:54:28 +0200

    missing projects

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,9 @@
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+var belgiumCart = new ShoppingCart(new BelgiumShoppingCartPurchaseFactory());
+belgiumCart.CalculateCosts();
+
+var franceCart = new ShoppingCart(new FranceShoppingCartPurchaseFactory());
+franceCart.CalculateCosts();

68a6bea16b00771827fb476378640a6cb3c0868a
kristjankangro <87314450+kristjankangro@users.noreply.github.com>
2022-11-25 17:55:35 +0200

    strategy

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -5,11 +5,16 @@ Console.Title = "Strategy";
 var order = new Order("Ken", 12, "Watch");
 order.Export();
 
+Console.WriteLine("strategy:");
+Console.WriteLine();
+
 order.ExportService = new CsvExportService();
 order.Export();
 
 order.ExportService = new XmlExportService();
 order.Export();
 
+Console.WriteLine();
+Console.WriteLine("strategy variation:");
 Console.WriteLine();
 order.ExportVariation(new JsonExportService());

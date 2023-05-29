ac5e960b8131795394da4babef5e5d48be95c329
Kristjan <kristjan.kangro@visma.com>
2023-05-29 16:33:56 +0300

    branch

---
+++ _KristjanDev/Program.cs
@@ -0,0 +1,11 @@
+﻿using KristjanDev;
+
+Console.WriteLine("Hello, World!");
+
+var type1 = new Type1 { type1Key = "mingkey 1", type1Name = "mingi name 1" };
+var type2 = new Type2 { type2Key = "mingkey 22", type2Name = "mingi name 22" };
+var type3 = new Type3 { type3Key = "mingkey 333", type3Name = "mingi name 333" };
+
+Console.WriteLine(TranslationObjectService.GetObject(type1).Key);
+Console.WriteLine(TranslationObjectService.GetObject(type2).Key);
+Console.WriteLine(TranslationObjectService.GetObject(type3).Key);
---
+++ _KristjanDev/TranslationObjectSolution.cs
@@ -0,0 +1,74 @@
+namespace KristjanDev;
+
+public static class TranslationObjectService
+{
+    public static TranslationDto GetObject(ITranslationDtoGetter objectClass)
+    {
+        return objectClass.Get();
+    }
+}
+
+
+public class TranslationDto
+{
+    public string ET;
+    public string EN;
+    public string RU;
+    public string Key;
+}
+
+public interface ITranslationDtoGetter
+{
+    TranslationDto Get();
+}
+
+public class Type1 : ITranslationDtoGetter
+{
+    public string type1Name;
+    public string type1Key;
+
+    public TranslationDto Get()
+    {
+        return new TranslationDto
+        {
+            ET = type1Name,
+            EN = type1Name,
+            RU = type1Name,
+            Key = type1Key
+        };
+    }
+}
+
+public class Type2 : ITranslationDtoGetter
+{
+    public string type2Name;
+    public string type2Key;
+
+    public TranslationDto Get()
+    {
+        return new TranslationDto
+        {
+            ET = type2Name,
+            EN = type2Name,
+            RU = type2Name,
+            Key = type2Key
+        };
+    }
+}
+
+public class Type3 : ITranslationDtoGetter
+{
+    public string type3Name;
+    public string type3Key;
+
+    public TranslationDto Get()
+    {
+        return new TranslationDto
+        {
+            ET = type3Name,
+            EN = type3Name,
+            RU = type3Name,
+            Key = type3Key
+        };
+    }
+}

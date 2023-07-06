9186f23c12cd82bf20e9422dc21ff1d2b8263c3e
Kristjan <kristjan.kangro@visma.com>
2023-07-06 10:20:06 +0300

    singleton namespace

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,18 +1,19 @@
-﻿namespace Singleton
+﻿namespace Singleton;
+
+/*main muudatus singletoni namespace*/
+
+public class Logger
 {
-    public class Logger
+    private static Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+    protected Logger()
     {
-        private static Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+    }
 
-        protected Logger()
-        {
-        }
+    public static Logger Instance => _lazyLogger.Value;
 
-        public static Logger Instance => _lazyLogger.Value;
-
-        public void Log(string message)
-        {
-            Console.WriteLine($"Message to log: {message} ");
-        }
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message} ");
     }
 }
